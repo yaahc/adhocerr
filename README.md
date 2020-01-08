dc15d796339e7e7af017361a3bdc046d5a11ac8b
Jane Lusby <jlusby@yaah.dev>
2020-01-07 19:19:07 -0800

    document the crate

---
+++ README.md
@@ -0,0 +1,60 @@
+Adhoc Errors
+============
+
+[![Latest Version](https://img.shields.io/crates/v/adhocerr.svg)](https://crates.io/crates/adhocerr)
+[![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/adhocerr)
+
+A library for the construction of efficient single use static/dynamic error types per callsite.
+
+
+```toml
+[dependencies]
+adhocerr = "0.1"
+```
+
+<br>
+
+## Examples
+
+Creating an root cause error:
+
+```rust
+use std::error::Error;
+
+fn get_git_root(start: &Path) -> Result<PathBuf, impl Error + 'static> {
+    start
+        .ancestors()
+        .find(|a| a.join(".git").is_dir())
+        .map(Path::to_owned)
+        .ok_or(err!("Unable to find .git/ in parent directories"))
+}
+```
+
+Wrapping another Error:
+
+```rust
+use std::error::Error;
+
+fn record_success(tmp_commit: &SHA1) -> Result<(), impl Error + 'static> {
+    std::fs::write(".git/.gsync.sha" , tmp_commit)
+        .map_err(wrap!("Failed to save results of script"))
+}
+```
+
+<br>
+
+#### License
+
+<sup>
+Licensed under either of <a href="LICENSE-APACHE">Apache License, Version
+2.0</a> or <a href="LICENSE-MIT">MIT license</a> at your option.
+</sup>
+
+<br>
+
+<sub>
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in this crate by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
+</sub>
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,179 @@
+//! Adhoc Errors
+//! ============
+//!
+//! A library for the construction of efficient static/dynamic single use error types per callsite.
+//!
+//!
+//! ```toml
+//! [dependencies]
+//! adhocerr = "0.1"
+//! ```
+//!
+//! <br>
+//!
+//! ## Examples
+//!
+//! Creating an root cause error:
+//!
+//! ```rust
+//! use std::error::Error;
+//!
+//! fn get_git_root(start: &Path) -> Result<PathBuf, impl Error + 'static> {
+//!     start
+//!         .ancestors()
+//!         .find(|a| a.join(".git").is_dir())
+//!         .map(Path::to_owned)
+//!         .ok_or(err!("Unable to find .git/ in parent directories"))
+//! }
+//! ```
+//!
+//! Wrapping another Error:
+//!
+//! ```rust
+//! use std::error::Error;
+//!
+//! fn record_success() -> Result<(), impl Error + 'static> {
+//!     std::fs::write(".success", "true").map_err(wrap!("Failed to save results of script"))
+//! }
+//! ```
+//!
+//!
+//! <br>
+//!
+//! ## Details
+//!
+//! This crate provides two primary macros. `err!` and `wrap!`. The former, `err!`,
+//! is used to create adhoc error types without a root cause from strings. `wrap!`
+//! on the other hand is used to create new errors with a source member.
+//!
+//! Both of these macros have two versions, and they generate completely code,
+//! depending on whether or not string interopoation (`format!`-like code) is used
+//! in the error message. When the error message is a fixed string, the macro
+//! declares a new struct in line that has the string itself inserted into its
+//! `Display` implementation. This way no memory is used or allocations made to
+//! hold the error when they are not needed.
+//!
+//! For `err!` this means that your error type is a Zero Sized Type (ZST), for
+//! `wrap!` this means that your Wrapper error is the same size as the original
+//! error you're wrapping.
+//!
+//! When runtime interpolation is used and a `String` allocation is necessary it
+//! uses pre defined Error types to wrap the String to avoid declaring new types
+//! unnecessarily, but hides them behind an impl Trait boundary.
+//!
+//! ### Expanded
+//!
+//! The Expanded version of the example above would look like this:
+//!
+//! ```rust
+//! fn get_git_root(start: &Path) -> Result<PathBuf, impl Error + 'static> {
+//!     start
+//!         .ancestors()
+//!         .find(|a| a.join(".git").is_dir())
+//!         .map(Path::to_owned)
+//!         .ok_or({
+//!             #[derive(Debug)]
+//!             struct AdhocError;
+//!
+//!             impl std::error::Error for AdhocError {
+//!                 fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+//!                     None
+//!                 }
+//!             }
+//!
+//!             impl core::fmt::Display for AdhocError {
+//!                 fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+//!                     f.write_str("Unable to find .git/ in parent directories")
+//!                 }
+//!             }
+//!
+//!             AdhocError
+//!         })
+//! }
+//! ```
+use core::fmt;
 pub use err as format_err;
-use std::fmt;
 
 /// Thinly wrap an error by defining a hidden error type and returning a closure to construct it
+///
+/// ## Examples
+///
+/// Wrap an error without changing its size or allocating:
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// fn record_success() -> Result<(), impl Error + 'static> {
+///     std::fs::write(".success", "true").map_err(wrap!("Failed to save results of script"))
+/// }
+/// ```
+///
+/// Which expands to:
+///
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// fn record_success() -> Result<(), impl Error + 'static> {
+///     std::fs::write(".success", "true").map_err({
+///         #[derive(Debug)]
+///         struct WrappedError<E> {
+///             source: E,
+///         }
+///
+///         impl<E> std::error::Error for WrappedError<E>
+///         where
+///             E: std::error::Error + 'static,
+///         {
+///             fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+///                 Some(&self.source)
+///             }
+///         }
+///
+///         impl<E> core::fmt::Display for WrappedError<E>
+///         where
+///             E: std::error::Error + 'static,
+///         {
+///             fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+///                 f.write_str("Failed to save results of script")
+///             }
+///         }
+///
+///         |source| WrappedError { source }
+///     })
+/// }
+/// ```
+///
+/// Wrapping an error with an runtime generated String:
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// fn record_success(file: &Path) -> Result<(), impl Error + 'static> {
+///     std::fs::write(file, "true").map_err(wrap!(
+///         "Failed to save results of script to file: {}",
+///         file.display()
+///     ))
+/// }
+/// ```
+///
+/// Which expands to:
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// fn record_success(file: &Path) -> Result<(), impl Error + 'static> {
+///     std::fs::write(file, "true").map_err(|source| {
+///         crate::private::format_wrap_err(
+///             source,
+///             format_args!(
+///                 "Failed to save results of script to file: {}",
+///                 file.display()
+///             ),
+///         )
+///     })
+/// }
+/// ```
 #[macro_export]
 macro_rules! wrap {
     ($msg:literal) => {{
@@ -19,11 +191,11 @@ macro_rules! wrap {
             }
         }
 
-        impl<E> std::fmt::Display for WrappedError<E>
+        impl<E> core::fmt::Display for WrappedError<E>
         where
             E: std::error::Error + 'static,
         {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
                 f.write_str($msg)
             }
         }
@@ -36,6 +208,85 @@ macro_rules! wrap {
 }
 
 /// Create an adhoc error type with zero size if none is needed
+///
+/// ## Examples
+///
+/// Creating a static adhoc error type:
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// fn get_git_root(start: &Path) -> Result<PathBuf, impl Error + 'static> {
+///     start
+///         .ancestors()
+///         .find(|a| a.join(".git").is_dir())
+///         .map(Path::to_owned)
+///         .ok_or(err!("Unable to find .git/ in parent directories"))
+/// }
+/// ```
+///
+/// Which expands to:
+///
+/// ```rust
+/// fn get_git_root(start: &Path) -> Result<PathBuf, impl Error + 'static> {
+///     start
+///         .ancestors()
+///         .find(|a| a.join(".git").is_dir())
+///         .map(Path::to_owned)
+///         .ok_or({
+///             #[derive(Debug)]
+///             struct AdhocError;
+///
+///             impl std::error::Error for AdhocError {
+///                 fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+///                     None
+///                 }
+///             }
+///
+///             impl core::fmt::Display for AdhocError {
+///                 fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+///                     f.write_str("Unable to find .git/ in parent directories")
+///                 }
+///             }
+///
+///             AdhocError
+///         })
+/// }
+/// ```
+///
+/// Creating a dynamic adhoc error type:
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// fn get_git_root(start: &Path) -> Result<PathBuf, impl Error + 'static> {
+///     start
+///         .ancestors()
+///         .find(|a| a.join(".git").is_dir())
+///         .map(Path::to_owned)
+///         .ok_or(err!(
+///             "Unable to find .git/ in parent directories for {}",
+///             start.display()
+///         ))
+/// }
+/// ```
+///
+/// Which expands to:
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// fn get_git_root(start: &Path) -> Result<PathBuf, impl Error + 'static> {
+///     start
+///         .ancestors()
+///         .find(|a| a.join(".git").is_dir())
+///         .map(Path::to_owned)
+///         .ok_or(crate::private::format_err(format_args!(
+///             "Unable to find .git/ in parent directories for {}",
+///             start.display()
+///         )))
+/// }
+/// ```
 #[macro_export]
 macro_rules! err {
     ($msg:literal) => {{
@@ -48,8 +299,8 @@ macro_rules! err {
             }
         }
 
-        impl std::fmt::Display for AdhocError {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        impl core::fmt::Display for AdhocError {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
                 f.write_str($msg)
             }
         }
@@ -97,6 +348,7 @@ where
     }
 }
 
+/// Return an adhoc error if the boolean is false
 #[macro_export]
 macro_rules! ensure {
     ($cond:expr, $msg:literal $(,)?) => {
@@ -111,13 +363,14 @@ macro_rules! ensure {
     };
 }
 
+/// Return an adhoc error immediately
 #[macro_export]
 macro_rules! bail {
     ($msg:literal $(,)?) => {
         $crate::private::Err($crate::err!($msg))?;
     };
     ($fmt:literal, $($arg:tt)*) => {
-        $crate::private::Err($crate::err!($fmt, $($arg)*))?;
+        $crate::private::Err($crate::err!($fmt, $($arg)*));
     };
 }
 
@@ -126,14 +379,14 @@ pub mod private {
     pub use core::result::Result::Err;
 
     pub fn format_err(
-        args: std::fmt::Arguments<'_>,
+        args: core::fmt::Arguments<'_>,
     ) -> impl std::error::Error + Send + Sync + 'static {
         crate::FormatError(args.to_string())
     }
 
     pub fn format_wrap_err(
         source: impl std::error::Error + Send + Sync + 'static,
-        args: std::fmt::Arguments<'_>,
+        args: core::fmt::Arguments<'_>,
     ) -> impl std::error::Error + Send + Sync + 'static {
         crate::FormatWrappedError {
             msg: args.to_string(),
